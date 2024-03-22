acd34891c3faf39f52d6eea36078f886cea9e7ce
sw <sebastianwach22@gmail.com>
2024-03-22 22:56:17 +0100

    Copy from cpp template

---
+++ module_config.cmake
@@ -0,0 +1,11 @@
+set( TARGET_NAME app )
+
+list(APPEND MODULE_DEPS
+    -pthread
+    -lfmt
+    )
+
+
+set( MAJOR 0 )
+set( MINOR 1 )
+set( PATCH 0 )
---
+++ sources/src/app/app.cpp
@@ -0,0 +1,13 @@
+#include "app/app.hpp"
+#include "config/config.hpp"
+#include "device/device.hpp"
+#include <iostream>
+#include <fmt/core.h>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    fmt::print("Hello, world!\n");
+    
+    return 0;
+}
